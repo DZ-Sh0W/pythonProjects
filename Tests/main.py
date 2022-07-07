aaccb3201fcfe42f5aa176e662949328d203cce4
WaLiD <walid.dekmous13@gmail.com>
2022-07-07 18:28:46 +0100

    Made changes

---
+++ Tests/main.py
@@ -0,0 +1,10 @@
+a = 'a'
+b = 1234
+
+email = input("enter email : ")
+password = input("enter password : ")
+
+if email == a and password == str(b):
+    print("Success !")
+else:
+    print('Fail !')
