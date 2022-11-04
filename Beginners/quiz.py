3f18983c163e4d9583b7a3f0e42fc11635f971be
WaLiD <walid.dekmous13@gmail.com>
2022-11-04 16:08:15 +0100

    Made changes

--- Beginners/quiz.py
+++ Beginners/quiz.py
@@ -6,44 +6,47 @@ y = "Yes"
 n = "No"
 
 # Question 1
-q1 = print("One Piece is trash or not ?")
+q1 = print("Apple is a US company ?")
 a1 = str(input("Answer 1 : "))
-if a1 == n :
+if a1 == y:
  p1 = 5
-elif a1 == y :
+elif a1 == n:
  p1 = -1
-else :
- print("Nta 7mar !")
+else:
+ print("Repeat !")
 print()
+
 # Question 2
-q2 = print("Game of Thrones is the best series in the world ?")
+q2 = print("There is 5 countries ?")
 a2 = str(input("Answer 2 : "))
-if a2 == y :
+if a2 == y:
  p2 = 5
-elif a2 == n :
+elif a2 == n:
  p2 = -1
-else :
- print("Nta 7mar !")
+else:
+ print("Repeat !")
 print()
+
 # Question 3
-q3 = print("Is HK Gay or not ?")
+q3 = print("Africa is the big country ?")
 a3 = str(input("Answer 3 : "))
-if a3 == y :
+if a3 == y:
  p3 = 5
-elif a3 == n :
+elif a3 == n:
  p3 = -1
-else :
- print("Nta 7mar !")
+else:
+ print("Repeat !")
 print()
+
 # Question 4
-q4 = print("Hamoda 7mar ?")
+q4 = print("The earth is round ?")
 a4 = str(input("Answer 4 : "))
-if a4 == y :
+if a4 == y:
  p4 = 5
-elif a4 == n :
+elif a4 == n:
  p4 = -1
-else :
- print("Nta 7mar !")
+else:
+ print("Repeat !")
 print()
 
 points = p1+p2+p3+p4
